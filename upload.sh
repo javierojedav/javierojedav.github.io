3a60abad84c47ee0c5f07d6f99aab2ecbab2253a
Javier Ojeda <113691956+javierojedav@users.noreply.github.com>
2025-07-05 12:08:42 -0400

    Delete upload.sh

--- upload.sh
+++
@@ -1,6 +0,0 @@
-scp -oHostKeyAlgorithms=+ssh-dss *.html jojeda@dgf.uchile.cl:/srv/users/tsol/mgf/jojeda/public_html
-
-scp -oHostKeyAlgorithms=+ssh-dss ./images/icon/*.png jojeda@dgf.uchile.cl:/srv/users/tsol/mgf/jojeda/public_html/images/icon/
-
-#data
-# /srv/users/tsol/mgf/jojeda/public_html/data/Conferences
